870760fa45ba691dd6a9622dd2437e50dc9793d2
Alex Cawood-Hunt <alex.cawood-hunt@gigaclear.com>
2020-07-28 22:13:01 +0100

    NMZ plugin v1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Example
-An example greeter plugin
+# NMZ Optimal Points
+A plugin that uses coloured highlights and text to show players the optimal enemy to attack in NMZ.
+
+Code is adapted from the NPC Highlight plugin.
+
+Known issues:
+- The normal scores for the Corrupt Lizardman, Elven Traitor and Essyllt are not listed on the wiki, so are not currently accurate           
+- The highlighting doesn't work on Nazastarool's additonal forms         
--- src/test/java/com/optimalpoints/OptimalPointsPluginTest.java
+++ src/test/java/com/optimalpoints/OptimalPointsPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.optimalpoints;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OptimalPointsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OptimalPointsPlugin.class);
 		RuneLite.main(args);
 	}
 }
